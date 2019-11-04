25d1c7f56d965c5d7715bfcfd4cc935c1b20c24c
Jon Friesen <jfriesen@digitalocean.com>
2019-11-04 09:33:38 -0800

    adds endpoint option to S3 cp

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,7 @@ aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
 aws s3 cp ${FILE} s3://${AWS_S3_BUCKET} \
+            --endpoint-url ${AWS_ENDPOINT:-amazonaws.com}
             --region ${AWS_REGION} $*
 
 rm -rf ~/.aws
